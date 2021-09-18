e7aff0c324326909b9a4e901514d7354ba0ba3f9
Imash Nidusha Fernando <90789073+IMASHBOT-W@users.noreply.github.com>
2021-09-19 01:46:31 +0530

    Update amalser.js

--- plugins/amalser.js
+++ plugins/amalser.js
@@ -46,7 +46,7 @@ notheri()
 var ldc = ''
 if (Config.LANG == 'AZ') ldc = '*Bağlantı Aşkarlandı!*'
 if (Config.LANG == 'TR') ldc = '*‎Link Tespit Edildi!*'
-if (Config.LANG == 'EN') ldc = '*Dont use badwords....* \n 😅 *Dont share other groups links here....* \n *No warning ⚠️ After kick i will say* 😅😅'
+if (Config.LANG == 'EN') ldc = '*Dont use badwords....* \n 😡 *Dont share other groups links here....* \n *No warning ⚠️ After kick i will say* 🥲😅'
 if (Config.LANG == 'ML') ldc = '*ലിങ്ക് കണ്ടെത്തി!*'
 if (Config.LANG == 'ID') ldc = '*Tautan Terdeteksi!*'
 if (Config.LANG == 'PT') ldc = '*Link Detectado!*'
